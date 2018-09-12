c8d0a4d9fd75545567dbdfd503b3b32a3259a260
sliu-01 <42978561+sliu-01@users.noreply.github.com>
2018-09-12 14:29:00 -0400

    Add files via upload

---
+++ src/SmileyFace.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author sTEVEN lIU
+ * Smiley Face
+ *
+ */
+public class SmileyFace {
+
+	public static void main(String[] args) {
+		System.out.println("    /*********\\  \n"
+				+ "   / ___   ___ \\\n"
+				+ " /*|  *     *  |*\\\n"
+				+ " \\_|     >     |_/\n"
+				+ "   \\  \\_____/  /\n"
+				+ "    \\  \\___/  /\n"
+				+ "     \\_______/\n");
+
+	}
+
+}
